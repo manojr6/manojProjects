8ce8388e561ec0d4cbc3e9444883db776fe11ba7
user <user@Manoj>
2023-07-27 13:03:34 +0530

    back to normal3

--- DDFviaMaven1/src/test/java/orangeHRM/loginOrangeHRM1.java
+++ DDFviaMaven1/src/test/java/orangeHRM/loginOrangeHRM1.java
@@ -153,7 +153,7 @@ public class loginOrangeHRM1  {
 
 	@AfterTest
 	public void afterTest() throws InterruptedException {
-		System.out.println("back to normal1");
+		System.out.println("back to normal3");
 		driver.quit(); 
 	}
 
